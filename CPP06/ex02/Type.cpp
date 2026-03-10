1ce710a51898d66f5cbe0d2bfe5a1280f6315980
Severyn Valchuk <svalchuk@student.42wolfsburg.de>
2026-03-10 19:23:00 +0100

    CPP06 | small tweaks

--- CPP06/ex02/Type.cpp
+++ CPP06/ex02/Type.cpp
@@ -5,52 +5,52 @@ Base* Type::generate(void) {
     
     switch (random) {
         case 0:
-            std::cout << GREEN << "A class was created" << std::endl;
+            std::cout << GREEN << "A class was created" << RESET << std::endl;
             return new A();
         case 1:
-            std::cout << CYAN << "B class was created" << std::endl;
+            std::cout << CYAN << "B class was created" << RESET << std::endl;
             return new B();
         default:
-            std::cout << PURPLE << "C class was created" << std::endl;
+            std::cout << PURPLE << "C class was created" << RESET << std::endl;
             return new C();
     }
 }
 
 void Type::indentify(Base* p) {
     if (p == NULL) {
-        std::cout << YELLOW << "Null pointer provided" << std::endl;
+        std::cout << YELLOW << "Null pointer provided" << RESET << std::endl;
         return;
     }
     
     if (dynamic_cast<A*>(p) != NULL) {
-        std::cout << GREEN << "A" << std::endl;
+        std::cout << GREEN << "A" << RESET << std::endl;
     } else if (dynamic_cast<B*>(p) != NULL) {
-        std::cout << CYAN << "B" << std::endl;
+        std::cout << CYAN << "B" << RESET << std::endl;
     } else if (dynamic_cast<C*>(p) != NULL) {
-        std::cout << PURPLE << "C" << std::endl;
+        std::cout << PURPLE << "C" << RESET << std::endl;
     } else {
-        std::cout << RED << "Unknown type" << std::endl;
+        std::cout << RED << "Unknown type" << RESET << std::endl;
     }
 }
 
 void Type::indentify(Base& p) {
     try {
         (void)dynamic_cast<A&>(p);
-        std::cout << GREEN << "A" << std::endl;
+        std::cout << GREEN << "A" << RESET << std::endl;
         return;
-    } catch (const std::bad_cast& e) {}
+    } catch (const std::exception& e) {}
     
     try {
         (void)dynamic_cast<B&>(p);
-        std::cout << CYAN << "B" << std::endl;
+        std::cout << CYAN << "B" << RESET << std::endl;
         return;
-    } catch (const std::bad_cast& e) {}
+    } catch (const std::exception& e) {}
     
     try {
         (void)dynamic_cast<C&>(p);
-        std::cout << PURPLE << "C" << std::endl;
+        std::cout << PURPLE << "C" << RESET << std::endl;
         return;
-    } catch (const std::bad_cast& e) {}
+    } catch (const std::exception& e) {}
     
-    std::cout << RED << "Unknown type" << std::endl;
+    std::cout << RED << "Unknown type" << RESET << std::endl;
 }
